a375c9bdc5b9b8256e518f68abe218640daf53bd
stecala <ste.cala123@gmail.com>
2022-07-15 14:47:12 +0200

    Feat ⭐ populating the array

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,78 @@
 <template>
-  <div class="hello">
-  
+  <div>
+      <div class="container-lg">
+          <div class="row py-4 ">
+              <div class="col-5">
+                  <img src="../assets/img/dc-logo.png" alt="Main logo DC">
+              </div>
+              <div class="col-7">
+
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data: function(){
+      return{
+          navList : [
+          {
+            section : 'CHARACTERS',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'COMICS',
+            active : 'true',
+            url : '#',
+          },
+          {
+            section : 'MOVIE',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'TV',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'GAMES',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'COLLECTIBLES',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'VIDEOS',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'FANS',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'NEWS',
+            active : 'false',
+            url : '#',
+          },
+          {
+            section : 'SHOP',
+            active : 'false',
+            url : '#',
+          },
+          ]
+      }
+  }
 }
+
 </script>
 
 <style scoped lang="scss">
