16c88e5a2b4b9310d6bd283dc1e667a921a7202c
stecala <ste.cala123@gmail.com>
2022-07-15 14:15:31 +0200

    Feat ⭐ Importing the files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    
+    <HeaderApp />
+
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import HeaderApp from './components/Header.vue'
+import MainContentApp from './components/Main.vue'
+import FooterApp from './components/Footer.vue'
 
 export default {
 }
