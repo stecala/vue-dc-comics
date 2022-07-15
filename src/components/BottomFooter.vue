f21ee3fd09f6940a8740c8dc5e870ba29c52a525
stecala <ste.cala123@gmail.com>
2022-07-15 16:48:02 +0200

    feat ⭐ added dc logo on footer

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    ciao
+  <div >
+    
   </div>
 </template>
 
@@ -10,6 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
 
 </style>
--- src/components/TopFooter.vue
+++ src/components/TopFooter.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-    ciao
-  </div>
+    <div class="footer-container">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-6 img-bg">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data : function (){
+        return{
+        
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.footer-container {
+    width: 100%;
+    height: 500px;
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.img-bg {
+    background-image: url('../assets/img/dc-logo-bg.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 500px;
+}
 </style>
