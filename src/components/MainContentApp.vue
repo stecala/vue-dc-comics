0319b173b6244b18ff718fbcd25b61479a17aea4
stecala <ste.cala123@gmail.com>
2022-07-18 14:03:00 +0200

    feat ⭐ imported props and library is now working

--- src/components/MainContentApp.vue
+++ src/components/MainContentApp.vue
@@ -1,25 +1,118 @@
 <template>
   <main class="my-bg">
     <div class="container-lg py-5">
-      
+      <div class="row g-3">
+        <Card v-for="(card, index) in comicsList" :key="index"
+        :card="card"
+        />
+        <div class="btn-cont">
+          <a class="btn">LOAD MORE</a>
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+  import Card from './SingleCard.vue'
 export default {
-
+  components : {
+    Card,
+  },  
+  data: function () {
+    return {
+      comicsList: [
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          "price": "$19.99",
+          "series": "Action Comics",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          "price": "$3.99",
+          "series": "American Vampire 1976",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          "price": "$16.99",
+          "series": "Aquaman",
+          "type": "graphic novel"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          "price": "$2.99",
+          "series": "Batgirl",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          "price": "$3.99",
+          "series": "Batman",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://images-na.ssl-images-amazon.com/images/I/81HYpRwzAuL.jpg",
+          "price": "$2.99",
+          "series": "Batman Beyond",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          "price": "$3.99",
+          "series": "Batman/Superman",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+          "price": "$4.99",
+          "series": "Batman/Superman Annual",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          "price": "$5.99",
+          "series": "Batman: The Joker War Zone",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          "price": "$6.99",
+          "series": "Batman: Three Jokers",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          "price": "$4.99",
+          "series": "Batman: White Knight Presents: Harley Quinn",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          "price": "$16.99",
+          "series": "Catwoman",
+          "type": "graphic novel"
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/style/variables.scss";
-.my-bg{
+
+.my-bg {
   background-color: $bgDark;
-  height: 150px;
   width: 100%;
 }
-h2{
+
+h2 {
   color: $textColorPrimary;
+}
+.btn-container{
+
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="col-2">
+        <div class="img-container">
+            <img :src="card.thumb" :alt="card.series">
+        </div>
+        <div class="text-container py-2">
+            {{card.series}}
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        'card' : Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+div{
+    cursor: pointer;
+    .img-container{
+        width: 192px;
+        height: 200px;
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: top;
+        }
+    }
+    .text-container{
+        color: $textColorPrimary;
+        text-transform: uppercase;
+    }
+}
+
+</style>
